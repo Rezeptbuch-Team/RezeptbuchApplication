24677e2291600a56f468bb190e2b67bfcd0b3364
Julius <julius.goeler@gmail.com>
2024-11-05 15:01:44 +0100

    basic onlinerecipelistservice implementation

--- src/ApplicationCore/Common/Types/RecipeEntry.cs
+++ src/ApplicationCore/Common/Types/RecipeEntry.cs
@@ -1,5 +1,10 @@
 namespace ApplicationCore.Common.Types;
 
-public struct RecipeEntry {
-    
+public struct RecipeEntry(string hash, string title, string description, string imagePath, List<string> categories, int cookingTime) {
+    public readonly string hash = hash;
+    public readonly string title = title;
+    public readonly string description = description;
+    public readonly string imagePath = imagePath;
+    public readonly List<string> categories = categories;
+    public readonly int cookingTime = cookingTime;
 }
--- src/ApplicationCore/Model/OnlineRecipeListService.cs
+++ src/ApplicationCore/Model/OnlineRecipeListService.cs
@@ -6,6 +6,9 @@ namespace ApplicationCore.Model;
 public class OnlineRecipeListService : IOnlineRecipeListService
 {
     public List<RecipeEntry> GetOnlineRecipeList(Filter filter) {
-        return new List<RecipeEntry>();
+        List<string> categories = ["category1", "category2"];
+        RecipeEntry recipeEntry1 = new("hash", "title", "description", "imagePath", categories, 15);
+        RecipeEntry recipeEntry2 = new("hash2", "title2", "description2", "imagePath2", categories, 30);
+        return [recipeEntry1, recipeEntry2];
     }
 }
