19bd4798ea8e01c19b049055589e1132be4b33be
Luca <luca@die5grimms.de>
2024-11-17 18:13:01 +0100

    very basic version of list recipes (shows a static list of strings)

--- src/GUI/ViewModel/ListLocalRecipeViewModel.cs
+++ src/GUI/ViewModel/ListLocalRecipeViewModel.cs
@@ -1,6 +1,25 @@
+using System.Collections.ObjectModel;
+using ApplicationCore.Common.Types;
+using ApplicationCore.Interfaces;
+using CommunityToolkit.Mvvm.ComponentModel;
+
 namespace GUI.ViewModel;
 
-public class ListLocalRecipeViewModel
+public partial class ListLocalRecipeViewModel : ObservableObject
 {
+
+    public class Recipe(string Titel, string Description);
     
+    public ListLocalRecipeViewModel()
+    {
+        Recipes = new();
+        List<string> categories = new();
+        for (int i = 0; i < 10; i++)
+        {
+            Recipes.Add(new Recipe($"Titel {i}", $"Description {i}"));
+        }
+    }
+    
+    [ObservableProperty] 
+    private ObservableCollection<Recipe> recipes;
 }
