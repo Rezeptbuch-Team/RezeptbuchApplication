89f20e1e45d3c1d816a81234ee811b55dac6220a
Julius <julius.goeler@gmail.com>
2025-05-04 16:39:55 +0200

    implemented sqlite database handler

--- src/ApplicationCore/Model/SqliteService.cs
+++ src/ApplicationCore/Model/SqliteService.cs
@@ -1,34 +1,72 @@
+using System.Data;
+using System.Data.Common;
 using ApplicationCore.Interfaces;
-
 using Microsoft.Data.Sqlite;
 
 namespace ApplicationCore.Model;
 
+/// <summary>
+/// light-weight SqliteService
+/// <para>
+/// <see cref="InitializeAsync" /> must be called once before using the other methods
+/// </para>
+/// </summary>
 public class SqliteService : IDatabaseService {
-    private readonly string connectionString;
+    private readonly string _connectionString;
+    private readonly string _dbPath;
 
-    public SqliteService() {
-        string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "database.sqlite");
-        connectionString = $"Data Source={dbPath}";
-
-        string schemaFilePath = Path.Combine(AppContext.BaseDirectory, "Database", "Scripts", "schema.sql");
-        if (!File.Exists(dbPath)) {
-            CreateDatabaseFromSchema(schemaFilePath);
-        }
+    public SqliteService(string? dbPath = null) {
+        string defaultDbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "database.sqlite");
+        _dbPath = string.IsNullOrWhiteSpace(dbPath) ? defaultDbPath : dbPath;
+        _connectionString = $"Data Source={dbPath}";
     }
 
-    private void CreateDatabaseFromSchema(string schemaFilePath) {
+    public async Task InitializeAsync() {
+        // create database if it does not exist
+        if (File.Exists(_dbPath)) return;
+
+        string schemaFilePath = Path.Combine(AppContext.BaseDirectory, "Database", "Scripts", "schema.sql");
         if (!File.Exists(schemaFilePath)) {
             throw new FileNotFoundException("Schema file not found", schemaFilePath);
         }
 
-        string schemaSql = File.ReadAllText(schemaFilePath);
+        string schemaSql = await File.ReadAllTextAsync(schemaFilePath);
 
-        using SqliteConnection connection = new(connectionString);
-        connection.Open();
+        await using SqliteConnection connection = new(_connectionString);
+        await connection.OpenAsync();
 
-        using SqliteCommand command = connection.CreateCommand();
+        await using SqliteCommand command = connection.CreateCommand();
         command.CommandText = schemaSql;
-        command.ExecuteNonQuery();
+        await command.ExecuteNonQueryAsync();
+    }
+
+
+    /// <summary>
+    /// Execute INSERT/UPDATE/DELETE asynchronously
+    /// </summary>
+    /// <param name="commandText">sql command</param>
+    /// <returns>The number of rows inserted, updated, or deleted. -1 for SELECT statements.</returns>
+    public async Task<int> NonQueryAsync(string commandText) {
+        await using SqliteConnection connection = new(_connectionString);
+        await connection.OpenAsync();
+
+        await using SqliteCommand command = connection.CreateCommand();
+        command.CommandText = commandText;
+        return await command.ExecuteNonQueryAsync();
+    }
+
+    /// <summary>
+    /// Execute SELECT asynchronously
+    /// </summary>
+    /// <param name="queryText"></param>
+    /// <returns>DbDataReader (needs to be disposed of after use)</returns>
+    public async Task<DbDataReader> QueryAsync(string queryText) {
+        SqliteConnection connection = new(_connectionString);
+        await connection.OpenAsync();
+
+        SqliteCommand command = connection.CreateCommand();
+        command.CommandText = queryText;
+        // CommandBehavior.CloseConnection: automatically close the db connection, when DbDataReader is disposed
+        return await command.ExecuteReaderAsync(CommandBehavior.CloseConnection);
     }
 }
