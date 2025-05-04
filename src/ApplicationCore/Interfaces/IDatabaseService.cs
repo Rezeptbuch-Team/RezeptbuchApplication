80cb3305c9ab64f5dfe6f079710838c4b1278526
Julius <julius.goeler@gmail.com>
2025-05-04 18:02:16 +0200

    define interface IDatabaseService

--- src/ApplicationCore/Interfaces/IDatabaseService.cs
+++ src/ApplicationCore/Interfaces/IDatabaseService.cs
@@ -1,5 +1,9 @@
+using System.Data.Common;
+
 namespace ApplicationCore.Interfaces;
 
 public interface IDatabaseService {
-    
+    public Task InitializeAsync();
+    public Task<int> NonQueryAsync(string commandText);
+    public Task<DbDataReader> QueryAsync(string queryText);
 }
