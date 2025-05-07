000571e366a9dec8327cb2cc018012c8d32b59de
Julius <julius.goeler@gmail.com>
2025-05-07 20:40:40 +0200

    Refactor Filter class constructor to use default parameter values

--- src/ApplicationCore/Common/Types/Filter.cs
+++ src/ApplicationCore/Common/Types/Filter.cs
@@ -1,11 +1,11 @@
 namespace ApplicationCore.Common.Types;
 
-public class Filter(OrderBy? orderBy, Order? order, List<string>? categories, List<string>? availableIngredients, int? count, int? offset)
+public class Filter(OrderBy orderBy = OrderBy.TITLE, Order order = Order.ASCENDING, List<string>? categories = null, List<string>? availableIngredients = null, int count = 10, int offset = 0)
 {
-    public OrderBy orderBy = orderBy ?? OrderBy.TITLE;
-    public Order order = order ?? Order.ASCENDING;
+    public OrderBy orderBy = orderBy;
+    public Order order = order;
     public List<string> categories = categories ?? [];
     public List<string> availableIngredients = availableIngredients ?? [];
-    public int count = count ?? 10;
-    public int offset = offset ?? 0;
+    public int count = count;
+    public int offset = offset;
 }
