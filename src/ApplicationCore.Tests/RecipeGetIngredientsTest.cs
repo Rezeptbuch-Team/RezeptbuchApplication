94202ba1969288200e76a32329e4bea93dac00d8
Julius <julius.goeler@gmail.com>
2025-05-18 22:37:25 +0200

    implemented Recipe.GetIngredients

--- src/ApplicationCore.Tests/RecipeGetIngredientsTest.cs
+++ src/ApplicationCore.Tests/RecipeGetIngredientsTest.cs
@@ -24,27 +24,152 @@ public class RecipeGetIngredientsTests
         };
     }
 
+    public class IngredientComparer : IEqualityComparer<Ingredient>
+    {
+        public bool Equals(Ingredient x, Ingredient y)
+        {
+            return x.Name == y.Name && x.Unit == y.Unit && x.Amount == y.Amount;
+        }
+
+        public int GetHashCode(Ingredient obj)
+        {
+            return HashCode.Combine(obj.Name, obj.Unit, obj.Amount);
+        }
+    }
+
     [Test]
     public void GetIngredients_ShouldCorrectlyExtract_SimpleIngredients()
     {
-        throw new NotImplementedException();
+        baseRecipe.Instructions = [
+            new Instruction(){
+                Items = [
+                    new Ingredient{
+                        Name="water", Amount=600, Unit="ml"
+                    },
+                    new Ingredient{
+                        Name="pasta", Amount=200, Unit="g"
+                    }
+                ]
+            },
+            new Instruction(){
+                Items = [
+                    new Ingredient{
+                        Name="basil", Amount=3, Unit="pieces"
+                    }
+                ]
+            }
+        ];
+        List<Ingredient> expectedIngredients = [
+            new Ingredient{
+                Name="water", Amount=600, Unit="ml"
+            },
+            new Ingredient{
+                Name="pasta", Amount=200, Unit="g"
+            },
+            new Ingredient{
+                Name="basil", Amount=3, Unit="pieces"
+            }
+        ];
+        
+        Assert.That(baseRecipe.GetIngredients(), Is.EqualTo(expectedIngredients));
     }
 
     [Test]
     public void GetIngredients_ShouldCorrectlyExtract_SimpleIngredients_IfThereIsText()
     {
-        throw new NotImplementedException();
+        baseRecipe.Instructions = [
+            new Instruction(){
+                Items = [
+                    "Boil",
+                    new Ingredient{
+                        Name="water", Amount=600, Unit="ml"
+                    },
+                    "and then cook",
+                    new Ingredient{
+                        Name="pasta", Amount=200, Unit="g"
+                    },
+                    "until al dente."
+                ]
+            },
+            new Instruction(){
+                Items = [
+                    "Now add some",
+                    new Ingredient{
+                        Name="basil", Amount=3, Unit="pieces"
+                    },
+                    "and serve"
+                ]
+            }
+        ];
+        List<Ingredient> expectedIngredients = [
+            new Ingredient{
+                Name="water", Amount=600, Unit="ml"
+            },
+            new Ingredient{
+                Name="pasta", Amount=200, Unit="g"
+            },
+            new Ingredient{
+                Name="basil", Amount=3, Unit="pieces"
+            }
+        ];
+
+        Assert.That(baseRecipe.GetIngredients(), Is.EqualTo(expectedIngredients));
     }
 
     [Test]
     public void GetIngredients_ShouldCorrectlyExtract_DuplicateIngredients_WithDifferentUnits()
     {
-        throw new NotImplementedException();
+        baseRecipe.Instructions = [
+            new Instruction(){
+                Items = [
+                    new Ingredient{
+                        Name="water", Amount=600, Unit="ml"
+                    },
+                    new Ingredient{
+                        Name="water", Amount=1, Unit="l"
+                    }
+                ]
+            }
+        ];
+        List<Ingredient> expectedIngredients = [
+            new Ingredient{
+                Name="water", Amount=600, Unit="ml"
+            },
+            new Ingredient{
+                Name="water", Amount=1, Unit="l"
+            }
+        ];
+
+        Assert.That(baseRecipe.GetIngredients(), Is.EqualTo(expectedIngredients));
     }
     
     [Test]
     public void GetIngredients_ShouldCorrectlyExtract_DuplicateIngredients_AndSumAmount_WhenSameUnits()
     {
-        throw new NotImplementedException();
+        baseRecipe.Instructions = [
+            new Instruction(){
+                Items = [
+                    new Ingredient{
+                        Name="water", Amount=600, Unit="ml"
+                    },
+                    new Ingredient{
+                        Name="water", Amount=1, Unit="l"
+                    },
+                    new Ingredient{
+                        Name="water", Amount=200, Unit="ml"
+                    }
+                ]
+            }
+        ];
+        List<Ingredient> expectedIngredients = [
+            new Ingredient{
+                Name="water", Amount=800, Unit="ml"
+            },
+            new Ingredient{
+                Name="water", Amount=1, Unit="l"
+            }
+        ];
+
+        Assert.That(baseRecipe.GetIngredients(), Is.EqualTo(expectedIngredients));
     }
 }
--- src/ApplicationCore/Common/Types/Recipe.cs
+++ src/ApplicationCore/Common/Types/Recipe.cs
@@ -1,17 +1,21 @@
 namespace ApplicationCore.Common.Types;
 
-public struct Ingredient
+public class Ingredient
 {
-    public string Name { get; set; }
-    public int Amount { get; set; }
-    public string Unit { get; set; }
+    public required string Name { get; set; }
+    public required int Amount { get; set; }
+    public required string Unit { get; set; }
+
+    public override bool Equals(object obj) =>
+        obj is Ingredient i &&
+        Name == i.Name && Amount == i.Amount && Unit == i.Unit;
 }
 
-public struct Instruction {
+public class Instruction {
     /// <summary>
     /// A list of text and ingredients
     /// </summary>
-    public List<object> Items { get; set; }
+    public List<object> Items { get; set; } = [];
 }
 
 public class Recipe
@@ -24,4 +28,48 @@ public class Recipe
     public required int CookingTime { get; set; }
     public required List<string> Categories { get; set; }
     public List<Instruction> Instructions { get; set; } = [];
+
+    public List<Ingredient> GetIngredients()
+    {
+        List<Ingredient> ingredients = [];
+
+        foreach (Instruction instruction in Instructions)
+        {
+            foreach (object item in instruction.Items)
+            {
+                if (item is Ingredient ingredient)
+                {
+                    Ingredient? existingIngredient = ingredients
+                        .FirstOrDefault(i => new IngredientNameUnitComparer().Equals(i, ingredient));
+
+                    if (existingIngredient != null)
+                    {
+                        existingIngredient.Amount += ingredient.Amount;
+                    }
+                    else
+                    {
+                        ingredients.Add(ingredient);
+                    }
+                }
+            }
+        }
+        
+        return ingredients.GroupBy(ing => ing.Name)
+                            .SelectMany(group => group)
+                            .ToList();
+    }
+
+
+    public class IngredientNameUnitComparer : IEqualityComparer<Ingredient>
+    {
+        public bool Equals(Ingredient x, Ingredient y)
+        {
+            return x.Name == y.Name && x.Unit == y.Unit;
+        }
+
+        public int GetHashCode(Ingredient obj)
+        {
+            return HashCode.Combine(obj.Name, obj.Unit);
+        }
+    }
 }
