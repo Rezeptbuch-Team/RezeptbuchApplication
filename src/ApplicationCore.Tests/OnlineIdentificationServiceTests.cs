6d8828ef7c9a68665a53e1830bed29d9106f062c
Julius <julius.goeler@gmail.com>
2025-05-19 15:47:59 +0200

    implement Get and CreateUUID

---
+++ src/ApplicationCore.Tests/OnlineIdentificationServiceTests.cs
@@ -0,0 +1,175 @@
+using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Model;
+using Moq;
+
+namespace ApplicationCore.Tests;
+
+[TestFixture]
+public class OnlineIdentificationServiceTests
+{
+    /// <summary>
+    /// Remve extra whitespaces and new-lines from the SQL string
+    /// </summary>
+    /// <param name="sql"></param>
+    /// <returns></returns>
+    private static string NormalizeSql(string sql)
+    {
+        return string.Join(" ", sql.Split([' ', '\r', '\n', '\t'], StringSplitOptions.RemoveEmptyEntries));
+    }
+
+    [Test]
+    public async Task GetUUID_ShouldCorrectlyFormSql()
+    {
+        #region database mock
+        string expectedSql = @"SELECT value
+                                FROM app_info
+                                WHERE key = 'uuid';";
+        #region create a fake DataTable to simulate the database response
+        DataTable table = new();
+        table.Columns.Add("value", typeof(string));
+        table.Rows.Add("asd123");
+        DbDataReader fakeReader = table.CreateDataReader();
+        #endregion
+
+        #region mock database controller
+        var mockDatabaseService = new Mock<IDatabaseService>();
+        mockDatabaseService.Setup(db => db.QueryAsync(
+            It.Is<string>(s => NormalizeSql(s) == NormalizeSql(expectedSql)),
+            It.IsAny<IDictionary<string, object>?>()
+        )).ReturnsAsync(fakeReader).Verifiable();
+        #endregion
+        #endregion
+
+        OnlineIdentificationService service = new(mockDatabaseService.Object);
+        string? uuid = await service.GetUUID();
+
+        mockDatabaseService.Verify();
+    }
+
+    [Test]
+    public async Task GetUUID_ShouldReturnTrueAndUUID_IfExists()
+    {
+        #region database mock
+        string expectedSql = @"SELECT value
+                                FROM app_info
+                                WHERE key = 'uuid';";
+        #region create a fake DataTable to simulate the database response
+        DataTable table = new();
+        table.Columns.Add("value", typeof(string));
+        table.Rows.Add("asd123");
+        DbDataReader fakeReader = table.CreateDataReader();
+        #endregion
+
+        #region mock database controller
+        var mockDatabaseService = new Mock<IDatabaseService>();
+        mockDatabaseService.Setup(db => db.QueryAsync(
+            It.Is<string>(s => NormalizeSql(s) == NormalizeSql(expectedSql)),
+            It.IsAny<IDictionary<string, object>?>()
+        )).ReturnsAsync(fakeReader).Verifiable();
+        #endregion
+        #endregion
+
+        OnlineIdentificationService service = new(mockDatabaseService.Object);
+        string? uuid = await service.GetUUID();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(uuid, Is.EqualTo("asd123"));
+        });
+    }
+
+    [Test]
+    public async Task GetUUID_ShouldReturnFalse_IfNotExists()
+    {
+        #region database mock
+        string expectedSql = @"SELECT value
+                                FROM app_info
+                                WHERE key = 'uuid';";
+        #region create a fake DataTable to simulate the database response
+        DataTable table = new();
+        table.Columns.Add("value", typeof(string));
+        DbDataReader fakeReader = table.CreateDataReader();
+        #endregion
+
+        #region mock database controller
+        var mockDatabaseService = new Mock<IDatabaseService>();
+        mockDatabaseService.Setup(db => db.QueryAsync(
+            It.Is<string>(s => NormalizeSql(s) == NormalizeSql(expectedSql)),
+            It.IsAny<IDictionary<string, object>?>()
+        )).ReturnsAsync(fakeReader).Verifiable();
+        #endregion
+        #endregion
+
+        OnlineIdentificationService service = new(mockDatabaseService.Object);
+        string? uuid = await service.GetUUID();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(uuid, Is.Null);
+        });
+    }
+
+    [Test]
+    public async Task CreateUUID_ShouldReturnUUID_IfItExists()
+    {
+        #region database mock
+        #region create a fake DataTable to simulate the database response
+        DataTable table = new();
+        table.Columns.Add("value", typeof(string));
+        table.Rows.Add("123asd");
+        DbDataReader fakeReader = table.CreateDataReader();
+        #endregion
+
+        #region mock database controller
+        var mockDatabaseService = new Mock<IDatabaseService>();
+        mockDatabaseService.Setup(db => db.QueryAsync(
+            It.IsAny<string>(),
+            It.IsAny<IDictionary<string, object>?>()
+        )).ReturnsAsync(fakeReader).Verifiable();
+        #endregion
+        #endregion
+
+        OnlineIdentificationService service = new(mockDatabaseService.Object);
+        string? uuid = await service.CreateUUID();
+
+        Assert.That(uuid, Is.EqualTo("123asd"));
+    }
+
+    [Test]
+    public async Task CreateUUID_ShouldCreateAndReturnUUID_IfItDoesNotExist()
+    {
+        #region database mock
+        string expectedInsertSql = @"INSERT INTO app_info (key, value) VALUES ($key, $value);";
+
+        #region create a fake DataTable to simulate the database response
+        DataTable table = new();
+        table.Columns.Add("value", typeof(string));
+        DbDataReader fakeReader = table.CreateDataReader();
+        #endregion
+
+        #region mock database controller
+        var mockDatabaseService = new Mock<IDatabaseService>();
+        mockDatabaseService.Setup(db => db.QueryAsync(
+            It.IsAny<string>(),
+            It.IsAny<IDictionary<string, object>?>()
+        )).ReturnsAsync(fakeReader).Verifiable();
+        mockDatabaseService.Setup(db => db.NonQueryAsync(
+            It.Is<string>(s => NormalizeSql(s) == NormalizeSql(expectedInsertSql)),
+            It.Is<IDictionary<string, object>>(p =>
+                p.ContainsKey("$key") && p["$key"].Equals("uuid") &&
+                p.ContainsKey("$value")
+            )
+        )).ReturnsAsync(1).Verifiable();
+        #endregion
+        #endregion
+
+        OnlineIdentificationService service = new(mockDatabaseService.Object);
+        string? uuid = await service.CreateUUID();
+
+        Assert.That(uuid, Has.Length.GreaterThan(4));
+        mockDatabaseService.Verify();
+    }
+}
