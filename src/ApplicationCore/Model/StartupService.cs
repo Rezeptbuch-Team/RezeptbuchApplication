abb7e17e4fdf65da0a8071b36204f818067674c6
Julius <julius.goeler@gmail.com>
2025-05-25 12:26:20 +0200

    rename method AppDataFolder to CreateAppDataFolder for clarity

--- src/ApplicationCore/Model/StartupService.cs
+++ src/ApplicationCore/Model/StartupService.cs
@@ -2,7 +2,7 @@ namespace ApplicationCore.Model;
 
 public class StartupService
 {
-    public static void AppDataFolder()
+    public static void CreateAppDataFolder()
     {
         string dirPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Rezeptbuch");
         if (!Directory.Exists(dirPath)) Directory.CreateDirectory(dirPath);
