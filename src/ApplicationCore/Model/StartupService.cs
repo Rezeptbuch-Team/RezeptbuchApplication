783220fde8b794f02419ef16853f92d11bde9868
Julius <julius.goeler@gmail.com>
2025-05-25 17:24:04 +0200

    base implementation CheckForOrphanedFiles

--- src/ApplicationCore/Model/StartupService.cs
+++ src/ApplicationCore/Model/StartupService.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.Data.Common;
 using System.Xml.Linq;
 using ApplicationCore.Common.Types;
@@ -7,12 +8,7 @@ namespace ApplicationCore.Model;
 
 public class StartupService(IDatabaseService databaseService, IGetRecipeFromFileService getRecipeFromFileService)
 {
-    public static void CreateAppDataFolder()
-    {
-        string dirPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Rezeptbuch");
-        if (!Directory.Exists(dirPath)) Directory.CreateDirectory(dirPath);
-    }
-
+    #region Helper functions
     private async Task UpdateDatabaseWithModifiedHash(string hash, string updatedHash)
     {
         string sql = @"UPDATE recipes
@@ -38,27 +34,42 @@ public class StartupService(IDatabaseService databaseService, IGetRecipeFromFile
     }
 
     private async Task CheckForConflictInFile(string filePath, string databaseHash) {
-        Recipe recipe = await getRecipeFromFileService.GetRecipeFromFile(filePath);
-        string calculatedHash = recipe.CalculateHash();
-        // if hash of recipe does not fit entry in database
-        if (calculatedHash != databaseHash)
+        try
         {
-            await UpdateDatabaseWithModifiedHash(databaseHash, calculatedHash);
+            Recipe recipe = await getRecipeFromFileService.GetRecipeFromFile(filePath);
+            string calculatedHash = recipe.CalculateHash();
+            // if hash of recipe does not fit entry in database
+            if (calculatedHash != databaseHash)
+            {
+                await UpdateDatabaseWithModifiedHash(databaseHash, calculatedHash);
 
-            #region update <hash> in file
-            // doc.Root and hashElem will not be null, because that was just checked by the GetRecipeFromFileService
-            XDocument doc = XDocument.Load(filePath);
-            XElement hashElem = doc.Root!.Element("hash")!;
-            hashElem.Value = calculatedHash;
-            doc.Save(filePath);
-            #endregion
+                #region update <hash> in file
+                // doc.Root and hashElem will not be null, because that was just checked by the GetRecipeFromFileService
+                XDocument doc = XDocument.Load(filePath);
+                XElement hashElem = doc.Root!.Element("hash")!;
+                hashElem.Value = calculatedHash;
+                doc.Save(filePath);
+                #endregion
+            }
+        }
+        catch
+        {
+            // we do not care if there are errors during this
+        }
+    }
+
+    private static IEnumerable<string> XmlFilesInDirectory()
+    {
+        string appDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Rezeptbuch");
+        foreach (string filePath in Directory.EnumerateFiles(appDataPath, "*.xml", SearchOption.AllDirectories))
+        {
+            yield return filePath;
         }
     }
 
     private static string? FindFileWithHash(string hash)
     {
-        string appDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Rezeptbuch");
-        foreach (string filePath in Directory.EnumerateFiles(appDataPath, "*.xml", SearchOption.AllDirectories))
+        foreach (string filePath in XmlFilesInDirectory())
         {
             XDocument doc = XDocument.Load(filePath);
             XElement? hashElem = doc.Root?.Element("hash");
@@ -80,6 +91,46 @@ public class StartupService(IDatabaseService databaseService, IGetRecipeFromFile
         };
 
         await databaseService.NonQueryAsync(sql, parameters);
+    }
+
+
+    private async Task<bool> IsHashInDatabase(string hash)
+    {
+        string sql = @"SELECT COUNT(*)
+                        FROM recipes
+                        WHERE hash = $hash;";
+        Dictionary<string, object> parameters = new()
+        {
+            { "$hash", hash }
+        };
+        await using (DbDataReader reader = await databaseService.QueryAsync(sql, parameters))
+        {
+            return reader.HasRows;
+        }
+    }
+
+    private async Task AddRecipeToDatabase(Recipe recipe)
+    {
+        throw new NotImplementedException();
+        string insertRecipeSql = @"INSERT INTO recipes
+                                    ";
+        string insertCategoriesSql = @"";
+        string insertIngredientsSql = @"";
+
+        string sql = insertRecipeSql + insertCategoriesSql + insertIngredientsSql;
+        Dictionary<string, object> parameters = new()
+        {
+
+        };
+
+        await databaseService.NonQueryAsync(sql, parameters);
+    }
+    #endregion
+    
+    public static void CreateAppDataFolder()
+    {
+        string dirPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Rezeptbuch");
+        if (!Directory.Exists(dirPath)) Directory.CreateDirectory(dirPath);
     }
 
     public async Task CheckForConflicts()
@@ -127,13 +178,22 @@ public class StartupService(IDatabaseService databaseService, IGetRecipeFromFile
         }
     }
 
-    public static void CheckForOrphanedFiles()
+    public async Task CheckForOrphanedFiles()
     {
-        throw new NotImplementedException();
-    }
-
-    public static void CheckForPublishableUpdates()
-    {
-        throw new NotImplementedException();
+        foreach (string filePath in XmlFilesInDirectory())
+        {
+            try
+            {
+                Recipe recipe = await getRecipeFromFileService.GetRecipeFromFile(filePath);
+                if (!await IsHashInDatabase(recipe.Hash))
+                {
+                    await AddRecipeToDatabase(recipe);
+                }
+            }
+            catch
+            {
+                // probably means that there is an error loading the recipe which we do not care about
+            }
+        }
     }
 }
