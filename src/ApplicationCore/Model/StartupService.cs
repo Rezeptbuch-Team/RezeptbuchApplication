c791d1e1f7cc8ecfc17ae5b46e665f199f740d56
Julius <julius.goeler@gmail.com>
2025-05-25 12:37:41 +0200

    add basic methods

--- src/ApplicationCore/Model/StartupService.cs
+++ src/ApplicationCore/Model/StartupService.cs
@@ -7,4 +7,19 @@ public class StartupService
         string dirPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Rezeptbuch");
         if (!Directory.Exists(dirPath)) Directory.CreateDirectory(dirPath);
     }
+
+    public static void CheckForConflicts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void CheckForOrphanedFiles()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void CheckForPublishableUpdates()
+    {
+        throw new NotImplementedException();
+    }
 }
